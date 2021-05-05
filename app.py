9371edc6dd19e9689957f914d6b76dc21dcf5c0f
Katsala Mariia <ubuntovka@gmail.com>
2021-05-05 16:55:42 +0300

    Added a detailed view of news and a tab for doctors with the implementation of sorting by specialty and a detailed view of information for each doctor. Added a folder with photos of doctors.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from models import classes_for_models
-from datetime import datetime
+from sqlalchemy.orm import session
+
+from models import classes_for_models as models_class
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mariia:root@localhost/clinic'
@@ -9,30 +10,53 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Article(db.Model):
+class Doctor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    intro = db.Column(db.String(300), nullable=False)
-    text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    fio = db.Column(db.String(100), nullable=False)
+    specialty = db.Column(db.String(300), nullable=False)
+    position = db.Column(db.String(300), nullable=False)
+    experience = db.Column(db.Integer, nullable=False)
+    photo = db.Column(db.LargeBinary)
 
     def __repr__(self):
-        return '<Article %r>' % self.id
+        return '<Doctor %r>' % self.id
 
 
 db.create_all()
 
 
 @app.route('/')
-@app.route("/home")
+@app.route('/home')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-@app.route("/posts")
+@app.route('/posts')
 def posts():
-    articles = Article.query.order_by(Article.date.desc()).all()
+    articles = models_class.Article.query.order_by(models_class.Article.date.desc()).all()
     return render_template('posts.html', articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = models_class.Article.query.get(id)
+    return render_template("post_detail.html", article=article)
+
+
+@app.route('/specialties')
+@app.route('/specialties/<sort_key>')
+def specialties(sort_key=None):
+    if sort_key is not None:
+        doctors = Doctor.query.where(Doctor.specialty == sort_key).all()
+    else:
+        doctors = Doctor.query.all()
+    return render_template('specialties.html', doctors=doctors)
+
+
+@app.route('/specialties/<int:id>')
+def doctor_detail(id):
+    doctor = Doctor.query.get(id)
+    return render_template("doctor_detail.html", doctor=doctor)
 
 
 if __name__ == "__main__":
