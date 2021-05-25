c888e1a0c84518872db6777a84c6b70d1dcf4cc4
Katsala Mariia <ubuntovka@gmail.com>
2021-05-25 22:08:46 +0300

    Added files that were skipped in the last push.

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mariia:root@localhost/clinic'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secret'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import models, views
+
+db.create_all()
